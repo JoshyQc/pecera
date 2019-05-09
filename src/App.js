3ba3c65f611d5b87ed422cbbfe997ec67f0d29cd
JoselyneQc <joselyne_carreraq@hotmail.com>
2019-05-09 15:05:45 -0600

    add new fuction to change the color and icons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const SERVER = "http://157.230.76.73:3000"; 
+const SERVER = "http://157.230.76.73:3000";
 
 class App extends React.Component {
 
@@ -10,58 +10,74 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      nombre: "BIENVENIDO AL CONTROL DE TU PECERA",
-      temp: 0,
-      ph: 2,
-      light: 32
+      nombre: "FISH TANK CONTROL",
+      temp: 5,
+      ph: 7,
+      light: 10
     }
 
   }
 
-  onAction = (e) => {
-    this.setState({
-      nombre: "WHAT?",
-    });
-  }
-
-  getNombre = () => {
-    return "Dinosaurio";
-  }
-
-  getInfo = () =>{
+  getInfo = () => {
     fetch(`${SERVER}/last`, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       }
-    }).then(response=>response.json()).then(response => {
+    }).then(response => response.json()).then(response => {
       this.setState({
         temp: response[0].temp,
         ph: response[0].ph,
         light: response[0].light,
       });
 
-      setTimeout(()=>{
+      setTimeout(() => {
         this.getInfo();
-      },1000);
+      }, 1000);
       console.log(response);
-  });
+    });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getInfo();
   }
 
-  // getColorPh = () =>{
-  //   if(ph == 60)
-  // }
+  getColorPh = () => {
+    const { ph } = this.state;
+
+    if (ph <= 1) {
+      return "#F44336";
+    }
+    if (ph <= 4) {
+      return "#FFA000";
+    }
+    if (ph <= 7) {
+      return "#4CAF50";
+    }
+    if (ph <= 12) {
+      return "#03A9F4";
+    }
+    if (ph <= 15) {
+      return "#9C27B0";
+    }
+  }
+
+  getColorTemp = () => {
+    const { temp } = this.state;
+
+    if (temp <= 5) {
+      return "#0288D1";
+    }
+    if (temp <= 30) {
+      return "#FF5722";
+    }
+  }
 
   render() {
     const { nombre } = this.state;
-    const { temp } = this.state;
-    const { ph } = this.state;
-    const { light } = this.state;
+    const { temp, ph, light } = this.state;
+
 
     return (
       <div className="container-fluid">
@@ -73,9 +89,9 @@ class App extends React.Component {
         <div className="row">
           <span className="glyphicon glyphicon-search" aria-hidden="true">
           </span>
-          <div className="col-md-4 temp">
+          <div className="col-md-4 temp" style={{ background: this.getColorTemp() }}>
 
-            <i className="material-icons icon">whatshot</i>
+            <i className="material-icons icon">{temp <= 5 ? 'ac_unit' : 'whatshot'}</i>
             <div className="info">
               <span className="titleCard">
                 TEMPERATURA
@@ -85,7 +101,7 @@ class App extends React.Component {
               </span>
             </div>
           </div>
-          <div className="col-md-4 ph">
+          <div className="col-md-4 ph" style={{ background: this.getColorPh() }}>
             <i className="material-icons icon">invert_colors</i>
             <div className="info">
               <span className="titleCard">
@@ -96,14 +112,14 @@ class App extends React.Component {
               </span>
             </div>
           </div>
-          <div className={light >= 490 ? 'col-md-4 light': 'col-md-4 dark' }>
+          <div className={light >= 490 ? 'col-md-4 light' : 'col-md-4 dark'}>
             <i className="material-icons icon">{light >= 490 ? 'brightness_5' : 'brightness_3'}</i>
             <div className="info">
               <span className="titleCard">
                 INTESIDAD DE LA LUZ
                 </span>
               <span className="valueCard">
-                {light}%
+                {light}
                 </span>
             </div>
           </div>
